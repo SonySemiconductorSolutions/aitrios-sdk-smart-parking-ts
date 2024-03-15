c28785063a901a0212e968993bcc5fd4df7703c5
TE-ChihiroIsmail <138419255+TE-ChihiroIsmail@users.noreply.github.com>
2024-03-15 16:59:12 +0900

    squash merge

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,22 @@
-FROM node:17.9.0
+FROM node:18-bullseye
 
 # Create app directory
 WORKDIR /usr/src/app
 
 # Install app dependencies
-RUN npm install react-scripts sass -g
+RUN npm install react-scripts@^5.0.1 sass@^1.70.0 -g
 
-COPY server ./server
-COPY web ./web
+COPY --chown=node:node server ./server
+COPY --chown=node:node web ./web
 
 WORKDIR /usr/src/app/web
 RUN npm install
-RUN npm run build
 
 WORKDIR /usr/src/app/server
 RUN npm install
+RUN npm run build-ui
+
+USER node
 
 # If you are building your code for production
 # RUN npm ci --only=production
--- web/src/pages/Layout/Layout.js
+++ web/src/pages/Layout/Layout.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Sony Semiconductor Solutions Corp. All rights reserved.
+ * Copyright 2023, 2024 Sony Semiconductor Solutions Corp. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import './styles.scss';
 
 import SmartParkingSVG from '../../components/shared/svg/SmartParkingSVG';
 import OfflineSVG from '../../components/shared/svg/OfflineSVG';
-import HelpSVG from '../../components/shared/svg/HelpSVG';
 
 const Layout = props => {
 
@@ -45,9 +44,6 @@ const Layout = props => {
                             </div>   
                         </div>
                     )}
-                    <div className="help__header__icon">
-                        <HelpSVG></HelpSVG>
-                    </div>
                 </div>
             </header>
             <div className="layout__content">
